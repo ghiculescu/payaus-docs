f1501080088e3360d821eac8e9a974e641299d10
Alex Ghiculescu <alexghiculescu@gmail.com>
2013-09-18 18:38:30 +1000

    add timesheet tags

--- _plugins/sorted_posts.rb
+++ _plugins/sorted_posts.rb
@@ -12,7 +12,7 @@ module Jekyll
       categories = {}
       category_order = site.config['sort_order']
       category_order.each do |category|
-        posts = site.posts.find_all {|p| (p.data['published'].nil? || p.data['published']) && p.data['categories'].include?(category)}.sort_by{|p| p.data['sort']}
+        posts = site.posts.find_all {|p| (p.data['published'].nil? || p.data['published']) && p.data['categories'].include?(category)}.sort_by{|p| (p.data['sort'] || 0)}
         categories[category] = posts unless posts.empty?
       end
       site.config['posts_sorted_by_category'] = categories.to_a
